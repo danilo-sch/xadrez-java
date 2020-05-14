2c68eba72e8879624fbbff1e80e3395b9519df98
Danilo Schmidt <danilo.97@hotmail.com>
2020-05-14 17:26:55 -0300

    Pecas e Tabuleirose print do tabuleiro

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,14 @@
 package application;
 
+import boardGame.Board;
+import boardGame.Position;
+import chess.ChessMatch;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Tesse");
+		ChessMatch cm = new ChessMatch();
+		UI.printBoard(cm.getPieces());
 
 	}
 
